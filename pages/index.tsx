e7cbfbe4db10b11aeb8ce1707e545df5c683b3ae
chawit rittiya <chawitmobile@gmail.com>
2022-06-15 23:24:41 +0700

    basic main page ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,14 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1>
-      Hello world!
-    </h1>
+    <div className='h-screen w-screen flex flex-col justify-center items-center gap-8'>
+      <div className='text-2xl text-center'>Which is roundest</div>
+      <div className='border rounded p-8 flex justify-between max-w-2xl items-center gap-8'>
+        <div className='w-16 h-16 bg-red-400 flex justify-center items-center'>test</div>
+        <div className='text-xl'>Vs</div>
+        <div className='w-16 h-16 bg-red-400 flex justify-center items-center'>test</div>
+      </div>
+    </div>
   )
 }
 
