15a803be2ff4c06c21a2bf741ae7420f3fd6b6cd
chawit rittiya <chawitmobile@gmail.com>
2022-06-15 22:57:38 +0700

    default main page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
+    <h1>
       Hello world!
     </h1>
   )
